7a8424a2732728757f8fc0b5c2ef916c0828a7bb
michael dye <m-github@divisive.info>
2017-03-23 21:38:28 -0600

    updated connection addresses

--- main.go
+++ main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"os"
+	"sync"
+	"time"
+
 	"github.com/golang/glog"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"io"
-
-	"sync"
-
-	"time"
-
-	"flag"
 
 	"github.com/michaeldye/synchrophasor-proto/pmu_server"
 	"github.com/michaeldye/synchrophasor-proto/synchrophasor_dpe"
 )
 
 const (
-	defaultPMUServerAddress           = "localhost:8008"
-	defaultSynchrophasorServerAddress = "localhost:9009"
+	defaultPMUServerAddress = "pmu:8008"
+	defaultDPEServerAddress = "dpe:9009"
 
 	// system won't drain until a buffer is full
 	bufferLength = 500
@@ -31,6 +30,16 @@ const (
 func main() {
 	flag.Parse()
 
+	pmuServerAddress := os.Getenv("PMU_SERVER_ADDRESS")
+	if pmuServerAddress == "" {
+		pmuServerAddress = defaultPMUServerAddress
+	}
+
+	dpeServerAddress := os.Getenv("DPE_SERVER_ADDRESS")
+	if dpeServerAddress == "" {
+		dpeServerAddress = defaultDPEServerAddress
+	}
+
 	// TODO: make a ring buffer of specific size
 	var cache [maxCache][]*pmu_server.SynchrophasorDatum
 	cacheMutex := &sync.Mutex{}
@@ -38,13 +47,13 @@ func main() {
 	// TODO: maybe do a defer, panic, recover?
 
 	for {
-		glog.Infof("Connecting to %v", defaultPMUServerAddress)
-		pmuClient, pmuConn, err := pmuConnect()
+		glog.Infof("Connecting to %v", pmuServerAddress)
+		pmuClient, pmuConn, err := pmuConnect(pmuServerAddress)
 		if err != nil {
 			glog.Errorf("Error connecting to PMU server: %v", err)
 		}
 
-		dpeClient, dpeConn, err := dpeConnect()
+		dpeClient, dpeConn, err := dpeConnect(dpeServerAddress)
 		if err != nil {
 			glog.Errorf("Error connecting to DPE server: %v", err)
 		}
@@ -69,8 +78,8 @@ func main() {
 }
 
 // TODO: consider using the gRPC backup connection lib here
-func pmuConnect() (pmu_server.SynchrophasorDataClient, *grpc.ClientConn, error) {
-	conn, err := grpc.Dial(defaultPMUServerAddress, grpc.WithInsecure(), grpc.WithTimeout(time.Duration(20)*time.Second))
+func pmuConnect(pmuServerAddress string) (pmu_server.SynchrophasorDataClient, *grpc.ClientConn, error) {
+	conn, err := grpc.Dial(pmuServerAddress, grpc.WithInsecure(), grpc.WithTimeout(time.Duration(20)*time.Second))
 	if err != nil {
 		return nil, nil, err
 	}
@@ -80,8 +89,8 @@ func pmuConnect() (pmu_server.SynchrophasorDataClient, *grpc.ClientConn, error) 
 }
 
 // TODO: reduce duplication between this connection initiation and the previous
-func dpeConnect() (synchrophasor_dpe.SynchrophasorDPEClient, *grpc.ClientConn, error) {
-	conn, err := grpc.Dial(defaultSynchrophasorServerAddress, grpc.WithInsecure(), grpc.WithTimeout(time.Duration(20)*time.Second))
+func dpeConnect(dpeServerAddress string) (synchrophasor_dpe.SynchrophasorDPEClient, *grpc.ClientConn, error) {
+	conn, err := grpc.Dial(dpeServerAddress, grpc.WithInsecure(), grpc.WithTimeout(time.Duration(20)*time.Second))
 	if err != nil {
 		return nil, nil, err
 	}
@@ -142,7 +151,6 @@ func store(datum *pmu_server.SynchrophasorDatum, cacheP *[maxCache][]*pmu_server
 
 	if cacheIdx < 0 {
 		glog.Errorf("Cache is at capacity (%v buckets of %v elements each), dropping record!!: %v", maxCache, bufferLength, datum.GetId())
-		panic("ouch!")
 	} else {
 		(*cacheP)[cacheIdx] = append((*cacheP)[cacheIdx], datum)
 		glog.V(6).Infof("Stored %v. Cache total: %v. Buffer total: %v", datum.GetId(), len(*cacheP), len((*cacheP)[cacheIdx]))
@@ -151,6 +159,10 @@ func store(datum *pmu_server.SynchrophasorDatum, cacheP *[maxCache][]*pmu_server
 }
 
 func send(client synchrophasor_dpe.SynchrophasorDPEClient, cacheP *[maxCache][]*pmu_server.SynchrophasorDatum, cacheMutex *sync.Mutex) {
+
+	lat := os.Getenv("HZN_LAT")
+	lon := os.Getenv("HZN_LON")
+	agreementID := os.Getenv("HZN_AGREEMENTID")
 
 	for {
 		// TODO: if no buckets are full, sleep; if buckets are full, send all full buckets then clear them (synchronize the read, unlock, send, then synchronize the clear)
@@ -185,8 +197,13 @@ func send(client synchrophasor_dpe.SynchrophasorDPEClient, cacheP *[maxCache][]*
 				glog.Fatalf("Error establishing stream to DPE server: %v", err)
 			}
 
-			for _, d := range *buffer {
-				err = stream.Send(d)
+			for _, datum := range *buffer {
+				err = stream.Send(&synchrophasor_dpe.HorizonDatumWrapper{
+					Lat:         lat,
+					Lon:         lon,
+					AgreementID: agreementID,
+					Datum:       datum,
+				})
 			}
 
 			if err != nil {
